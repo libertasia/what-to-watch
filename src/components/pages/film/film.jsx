ffc560a785f7ce2f2a6f7ce4a18c20944b28ec65
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2021-02-05 14:36:29 +0200

    Added logo and footer components, linked to the main page

--- src/components/pages/film/film.jsx
+++ src/components/pages/film/film.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PageFooter from '../../shared/page-footer/page-footer';
+import PageLogo from '../../shared/page-logo/page-logo';
 
 const Film = () => {
   return (
@@ -10,13 +12,7 @@ const Film = () => {
           </div>
           <h1 className="visually-hidden">WTW</h1>
           <header className="page-header movie-card__head">
-            <div className="logo">
-              <a href="main.html" className="logo__link">
-                <span className="logo__letter logo__letter--1">W</span>
-                <span className="logo__letter logo__letter--2">T</span>
-                <span className="logo__letter logo__letter--3">W</span>
-              </a>
-            </div>
+            <PageLogo />
             <div className="user-block">
               <div className="user-block__avatar">
                 <img src="img/avatar.jpg" alt="User avatar" width={63} height={63} />
@@ -122,18 +118,7 @@ const Film = () => {
             </article>
           </div>
         </section>
-        <footer className="page-footer">
-          <div className="logo">
-            <a href="main.html" className="logo__link logo__link--light">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-          <div className="copyright">
-            <p>© 2019 What to watch Ltd.</p>
-          </div>
-        </footer>
+        <PageFooter />
       </div>
     </React.Fragment>
   );
--- src/components/pages/my-list/my-list.jsx
+++ src/components/pages/my-list/my-list.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
+import PageFooter from '../../shared/page-footer/page-footer';
+import PageLogo from '../../shared/page-logo/page-logo';
 
 const MyList = () => {
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
-        <div className="logo">
-          <a href="main.html" className="logo__link">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
+        <PageLogo />
         <h1 className="page-title user-page__title">My list</h1>
         <div className="user-block">
           <div className="user-block__avatar">
@@ -95,18 +91,7 @@ const MyList = () => {
           </article>
         </div>
       </section>
-      <footer className="page-footer">
-        <div className="logo">
-          <a href="main.html" className="logo__link logo__link--light">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-        <div className="copyright">
-          <p>© 2019 What to watch Ltd.</p>
-        </div>
-      </footer>
+      <PageFooter />
     </div>
   );
 };
