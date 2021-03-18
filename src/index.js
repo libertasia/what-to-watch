f0cc830c59ed0f38fd805b0ad9b7af6c21b1b25d
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2021-03-18 19:48:46 +0200

    fixed browserHistory path in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router as BrowserRouter} from 'react-router-dom';
-import browserHistory from "../../browser-history";
+import browserHistory from "./browser-history";
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
