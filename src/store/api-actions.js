a4ada680fa6e22486710c5ce5edb685c4395eedc
Anastasiia Erokhina <erokhinaanastasiia@gmail.com>
2021-03-18 13:56:55 +0200

    Added tests for Async code

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -15,7 +15,7 @@ export const fetchFilmsList = () => (dispatch, _getState, api) => {
 
 export const fetchPromoFilm = () => (dispatch, _getState, api) => {
   dispatch(ActionCreator.setIsPromoLoading(true));
-  api.get(APIRoute.PROMO_FILM)
+  return api.get(APIRoute.PROMO_FILM)
     .then(({data}) => adaptFilmToClient(data))
     .then((film) => {
       dispatch(ActionCreator.loadPromoFilm(film, true));
@@ -65,7 +65,7 @@ export const logout = () => (dispatch, _getState, api) => (
 
 export const commentPost = (id, rating, comment) => (dispatch, _getState, api) => {
   dispatch(ActionCreator.setIsReviewFormDisabled(true));
-  api.post(`/comments/${id}`, {rating, comment})
+  return api.post(`/comments/${id}`, {rating, comment})
     .then(() => {
       dispatch(ActionCreator.redirectToRoute(`/films/${id}`));
       dispatch(ActionCreator.setIsReviewFormDisabled(false));
--- src/store/api-actions.test.js
+++ src/store/api-actions.test.js
@@ -1,8 +1,8 @@
 import MockAdapter from 'axios-mock-adapter';
 import {createAPI} from '../services/api';
 import {ActionType} from './action';
-import {fetchFilmsList} from './api-actions';
-import {APIRoute} from '../const';
+import {changeFavoriteStatus, checkAuth, commentPost, fetchFavoriteFilmsList, fetchFilmById, fetchFilmsList, fetchPromoFilm, fetchReviewsById, login, logout} from './api-actions';
+import {APIRoute, AppRoute, AuthorizationStatus} from '../const';
 
 const api = createAPI(() => {});
 
@@ -73,6 +73,363 @@ describe(`Async operation work correctly`, () => {
       expect(dispatch).toHaveBeenNthCalledWith(3, {
         type: ActionType.SET_IS_FILMS_LIST_LOADING,
         payload: false,
+      });
+    });
+  });
+  it(`Should make a correct API call to /films/promo`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const promoLoader = fetchPromoFilm();
+
+    apiMock
+      .onGet(APIRoute.PROMO_FILM)
+      .reply(200,
+          {
+            "name": `test`,
+            "poster_image": `test`,
+            "preview_image": `test`,
+            "background_image": `test`,
+            "background_color": `test`,
+            "description": `test`,
+            "rating": 0,
+            "scores_count": 0,
+            "director": `test`,
+            "starring": [],
+            "run_time": 0,
+            "genre": `test`,
+            "released": 0,
+            "id": 1,
+            "is_favorite": false,
+            "video_link": `test`,
+            "preview_video_link": `test`
+          }
+      );
+
+    return promoLoader(dispatch, () => {}, api)
+    .then(() => {
+      expect(dispatch).toHaveBeenCalledTimes(3);
+      expect(dispatch).toHaveBeenNthCalledWith(1, {
+        type: ActionType.SET_IS_PROMO_LOADING,
+        payload: true,
+      });
+      expect(dispatch).toHaveBeenNthCalledWith(2, {
+        type: ActionType.LOAD_PROMO_FILM,
+        payload: {
+          film:
+            {
+              id: 1,
+              name: `test`,
+              rating: 0,
+              description: `test`,
+              director: `test`,
+              starring: [],
+              genre: `test`,
+              released: 0,
+              posterImage: `test`,
+              previewImage: `test`,
+              backgroundImage: `test`,
+              backgroundColor: `test`,
+              scoresCount: 0,
+              runTime: 0,
+              isFavorite: false,
+              videoLink: `test`,
+              previewVideoLink: `test`,
+            },
+          isPromoLoaded: true,
+        }
+      });
+      expect(dispatch).toHaveBeenNthCalledWith(3, {
+        type: ActionType.SET_IS_PROMO_LOADING,
+        payload: false,
+      });
+    });
+  });
+
+  it(`Should make a correct API call to /favorite`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const favoriteFilmsLoader = fetchFavoriteFilmsList();
+
+    apiMock
+      .onGet(APIRoute.FAVORITE_FILMS)
+      .reply(200, [
+        {
+          "name": `test`,
+          "poster_image": `test`,
+          "preview_image": `test`,
+          "background_image": `test`,
+          "background_color": `test`,
+          "description": `test`,
+          "rating": 0,
+          "scores_count": 0,
+          "director": `test`,
+          "starring": [],
+          "run_time": 0,
+          "genre": `test`,
+          "released": 0,
+          "id": 1,
+          "is_favorite": false,
+          "video_link": `test`,
+          "preview_video_link": `test`
+        }
+      ]);
+
+    return favoriteFilmsLoader(dispatch, () => {}, api)
+    .then(() => {
+      expect(dispatch).toHaveBeenCalledTimes(1);
+      expect(dispatch).toHaveBeenNthCalledWith(1, {
+        type: ActionType.LOAD_FAVORITE_FILMS_LIST,
+        payload: [
+          {
+            id: 1,
+            name: `test`,
+            rating: 0,
+            description: `test`,
+            director: `test`,
+            starring: [],
+            genre: `test`,
+            released: 0,
+            posterImage: `test`,
+            previewImage: `test`,
+            backgroundImage: `test`,
+            backgroundColor: `test`,
+            scoresCount: 0,
+            runTime: 0,
+            isFavorite: false,
+            videoLink: `test`,
+            previewVideoLink: `test`,
+          }
+        ],
+      });
+    });
+  });
+  it(`Should make a correct API call to /films/:id`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const id = 1;
+    const filmLoader = fetchFilmById(id);
+
+    apiMock
+      .onGet(`/films/${id}`)
+      .reply(200,
+          {
+            "name": `test`,
+            "poster_image": `test`,
+            "preview_image": `test`,
+            "background_image": `test`,
+            "background_color": `test`,
+            "description": `test`,
+            "rating": 0,
+            "scores_count": 0,
+            "director": `test`,
+            "starring": [],
+            "run_time": 0,
+            "genre": `test`,
+            "released": 0,
+            "id": 1,
+            "is_favorite": false,
+            "video_link": `test`,
+            "preview_video_link": `test`
+          }
+      );
+
+    return filmLoader(dispatch, () => {}, api)
+    .then(() => {
+      expect(dispatch).toHaveBeenCalledTimes(1);
+      expect(dispatch).toHaveBeenNthCalledWith(1, {
+        type: ActionType.LOAD_FILM,
+        payload: {
+          film: {
+            id: 1,
+            name: `test`,
+            rating: 0,
+            description: `test`,
+            director: `test`,
+            starring: [],
+            genre: `test`,
+            released: 0,
+            posterImage: `test`,
+            previewImage: `test`,
+            backgroundImage: `test`,
+            backgroundColor: `test`,
+            scoresCount: 0,
+            runTime: 0,
+            isFavorite: false,
+            videoLink: `test`,
+            previewVideoLink: `test`,
+          },
+          isFilmLoaded: true,
+        }
+      });
+    });
+  });
+  it(`Should make a correct API call to /comments/:id`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const id = 1;
+    const reviewsLoader = fetchReviewsById(id);
+
+    apiMock
+      .onGet(`/comments/${id}`)
+      .reply(200, [{fake: true}]);
+
+    return reviewsLoader(dispatch, () => {}, api)
+    .then(() => {
+      expect(dispatch).toHaveBeenCalledTimes(1);
+      expect(dispatch).toHaveBeenNthCalledWith(1, {
+        type: ActionType.LOAD_REVIEWS,
+        payload: {
+          reviews: [{fake: true}],
+          isReviewsLoaded: true,
+        }
+      });
+    });
+  });
+  it(`Should make a correct API call to /login`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const checkAuthLoader = checkAuth();
+
+    apiMock
+      .onGet(APIRoute.LOGIN)
+      .reply(200, [{fake: true}]);
+
+    return checkAuthLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.REQUIRED_AUTHORIZATION,
+          payload: AuthorizationStatus.AUTH,
+        });
+      });
+  });
+  it(`Should make a correct API call to /login`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const fakeUser = {email: `test@test.ru`, password: `123456`};
+    const loginLoader = login(fakeUser);
+
+    apiMock
+      .onPost(APIRoute.LOGIN)
+      .reply(200, [{fake: true}]);
+
+    return loginLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(2);
+
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.REQUIRED_AUTHORIZATION,
+          payload: AuthorizationStatus.AUTH,
+        });
+
+        expect(dispatch).toHaveBeenNthCalledWith(2, {
+          type: ActionType.REDIRECT_TO_ROUTE,
+          payload: AppRoute.ROOT,
+        });
+      });
+  });
+  it(`Should make a correct API call to /logout`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const logoutLoader = logout();
+
+    apiMock
+      .onGet(APIRoute.LOGOUT)
+      .reply(200, [{fake: true}]);
+
+    return logoutLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.REQUIRED_AUTHORIZATION,
+          payload: AuthorizationStatus.NO_AUTH,
+        });
+      });
+  });
+  it(`Should make a correct API call to /comments/:id`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const id = 1;
+    const fakeRating = 8;
+    const fakeComment = `test`;
+    const commentPostLoader = commentPost(id, fakeRating, fakeComment);
+
+    apiMock
+      .onPost(`/comments/${id}`)
+      .reply(200, [{fake: true}]);
+
+    return commentPostLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(3);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.SET_IS_REVIEW_FORM_DISABLED,
+          payload: true,
+        });
+        expect(dispatch).toHaveBeenNthCalledWith(2, {
+          type: ActionType.REDIRECT_TO_ROUTE,
+          payload: `/films/${id}`,
+        });
+        expect(dispatch).toHaveBeenNthCalledWith(3, {
+          type: ActionType.SET_IS_REVIEW_FORM_DISABLED,
+          payload: false,
+        });
+      });
+  });
+  it(`Should make a correct API call to /favorite/:id/:status`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const id = 1;
+    const status = 1;
+    const changeFavoriteStatusLoader = changeFavoriteStatus(id, status);
+
+    apiMock
+      .onPost(`/favorite/${id}/${status}`)
+      .reply(200,
+          {
+            "name": `test`,
+            "poster_image": `test`,
+            "preview_image": `test`,
+            "background_image": `test`,
+            "background_color": `test`,
+            "description": `test`,
+            "rating": 0,
+            "scores_count": 0,
+            "director": `test`,
+            "starring": [],
+            "run_time": 0,
+            "genre": `test`,
+            "released": 0,
+            "id": 1,
+            "is_favorite": true,
+            "video_link": `test`,
+            "preview_video_link": `test`
+          }
+      );
+
+    return changeFavoriteStatusLoader(dispatch, () => {}, api)
+    .then(() => {
+      expect(dispatch).toHaveBeenCalledTimes(1);
+      expect(dispatch).toHaveBeenNthCalledWith(1, {
+        type: ActionType.SET_FAVORITE_STATUS,
+        payload: {
+          id: 1,
+          name: `test`,
+          rating: 0,
+          description: `test`,
+          director: `test`,
+          starring: [],
+          genre: `test`,
+          released: 0,
+          posterImage: `test`,
+          previewImage: `test`,
+          backgroundImage: `test`,
+          backgroundColor: `test`,
+          scoresCount: 0,
+          runTime: 0,
+          isFavorite: true,
+          videoLink: `test`,
+          previewVideoLink: `test`,
+        },
       });
     });
   });
